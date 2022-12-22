50b2bbd949e5c6b53de349be564f1c633d2d0681
FelixCDA <feixeth@gmail.com>
2022-12-22 16:28:16 +0100

    felix 1st commit :  Changes to be committed: 	modified:   out/production/jeudeloie-java/Dice_Throw.class 	modified:   out/production/jeudeloie-java/Main.class 	modified:   src/Dice_Throw.java 	modified:   src/Main.java

--- src/Dice_Throw.java
+++ src/Dice_Throw.java
@@ -1,16 +1,9 @@
-import java.util.Scanner;
 public class Dice_Throw {
-    public static void main(String[] args) {
-
-        Scanner input = new Scanner(System.in);
-        System.out.println("Try to throw dices ? (y/n)");
-        input.nextLine();
-        //if (input.nextLine = 'y') {}
+    public int dice ()
+ {
         int randomNumA = 1 + (int) (Math.random() * 6);
         int randomNumB = 1 + (int) (Math.random() * 6);
 
-        System.out.println("First dice score is " + randomNumA);
-        System.out.println("Second dice score is " + randomNumB);
-
+        return(randomNumA + randomNumB);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        for (int i = 1; i <= 10; i++) {
-            System.out.print("  " + i);
-        }
-        for (int i = 11; i <= 63; i++) {
-            System.out.print(" " + i);
-        }
-        System.out.println(" ");
-        int player = 5;
-        System.out.println(player);
+       int Dice = new Dice_Throw().dice();
+       System.out.println(Dice);
     }
 }
